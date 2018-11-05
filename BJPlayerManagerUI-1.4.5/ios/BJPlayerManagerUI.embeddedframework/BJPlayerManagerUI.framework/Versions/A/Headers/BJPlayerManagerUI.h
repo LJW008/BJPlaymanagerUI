cad6cd7db9182cc8497d0997bc1fff2b0288face
JW <23553036@qq.com>
2018-11-05 11:46:36 +0800

    1

---
+++ BJPlayerManagerUI-1.4.5/ios/BJPlayerManagerUI.embeddedframework/BJPlayerManagerUI.framework/Versions/A/Headers/BJPlayerManagerUI.h
@@ -0,0 +1,39 @@
+//
+//  BJPlayerManagerUI.h
+//  Pods
+//
+//  Created by 辛亚鹏 on 2017/6/2.
+//
+//
+
+#ifndef BJPlayerManagerUI_h
+#define BJPlayerManagerUI_h
+
+#pragma mark - avplayer
+#import "BJPUDisplayViewController.h"
+#import "BJPUFullViewController.h"
+#import "BJPUSmallViewController.h"
+#import "BJPUViewController.h"
+#import "BJPUViewControllerProtocol.h"
+
+#pragma mark - ijkplayer
+#import "BJPIJKDisplayViewController.h"
+#import "BJPIJKFullViewController.h"
+#import "BJPIJKSmallViewController.h"
+#import "BJPIJKViewController.h"
+
+#import "BJPUTheme.h"
+#import "BJPUAppearance.h"
+
+#import "BJPUDefinitionView.h"
+#import "BJPUFullRightView.h"
+#import "BJPUProgressView.h"
+#import "BJPURateView.h"
+#import "BJPUSliderView.h"
+
+#import "MBProgressHUD+bjp.h"
+
+FOUNDATION_EXPORT NSString * BJPlayerMangaerUIName(void);
+FOUNDATION_EXPORT NSString * BJPlayerMangaerUIVersion(void);
+
+#endif /* BJPlayerManagerUI_h */
